8d8c6a45bbf93f778d51916971c70594913e9e68
Yi Wei <31630096+yiwei-kiwi@users.noreply.github.com>
2018-04-24 22:26:12 -0700

    Define basic class methods.

--- MazeRunner/Tile.cpp
+++ MazeRunner/Tile.cpp
@@ -1,1 +1,32 @@
 #include "Tile.h"
+
+Tile::Tile(bool wall, bool trap) : wall(wall), trap(trap)
+{
+}
+
+void Tile::preformAction(Player &victim)
+{
+	//nothing, an emtpy tile does nothing
+}
+
+bool Tile::isWall() const
+{
+	return this->wall;
+}
+
+bool Tile::isTrap() const
+{
+	return this->trap;
+}
+
+WallTile::WallTile() : Tile(true, false)
+{
+}
+
+ResetTrap::ResetTrap() : Tile(false, true)
+{
+}
+
+void ResetTrap::preformAction(Player &victim)
+{
+}
--- MazeRunner/Tile.h
+++ MazeRunner/Tile.h
@@ -1,8 +1,25 @@
 #pragma once
-class Tile
-{
-public:
 
-private:
+class Player;
+class Tile {
+public:
+	Tile(bool wall = false, bool trap = false);
+	virtual void preformAction(Player &victim);
+	bool isWall() const;
+	bool isTrap() const;
+protected:
+	const bool wall;
+	const bool trap;
+};
+
+class WallTile : public Tile {
+public:
+	WallTile();
+};
+
+class ResetTrap : public Tile {
+public:
+	ResetTrap();
+	void preformAction(Player &victim);
 };
 
